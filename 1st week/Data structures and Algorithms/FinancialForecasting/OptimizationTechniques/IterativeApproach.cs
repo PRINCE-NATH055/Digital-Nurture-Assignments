d92b2cbe74f31ab358dd3efc32eb4ffa652319f2
PRINCE-NATH055 <princenath055@gmail.com>
2025-06-18 21:51:04 +0530

    Ouput image Added

--- 1st week/Data structures and Algorithms/FinancialForecasting/OptimizationTechniques/IterativeApproach.cs
+++ 1st week/Data structures and Algorithms/FinancialForecasting/OptimizationTechniques/IterativeApproach.cs
@@ -1,7 +1,6 @@
 /*the iterative approach avoids recursion entirely by using a simple loop.
  It’s more efficient because it doesn't use the call stack and operates in constant stack space,
- offering linear time complexity O(n) just like tail recursion,
- but with better performance and no risk of stack overflow*/
+ offering linear time complexity O(n), but with better performance and no risk of stack overflow*/
 
  namespace FinancialForecast.OptimizationTechniques
 {
